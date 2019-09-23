3e254c55a65aa1af6481f282a9258f276ec662d2
Peter Klopp <peterklopp02@gmail.com>
2019-09-22 19:34:01 -0700

    Nested Loops

--- Labs/Lab 918 - Nested Loops/NestedLoops.java
+++ Labs/Lab 918 - Nested Loops/NestedLoops.java
@@ -41,11 +41,71 @@ public class NestedLoops
         }
     }
     
+    //outer loop controls number of repetitions
+    //execute inner loop to completion every cycle
+    
     public static void problemD(int n){
-        for(int r = 7; r>=n; r--){
-            for(int c = 0; c<n; c++){
-                System.out.print("-"+ r);
+        for(int r = n; r>=1; r--){
+            for(int c = 0; c<r; c++){
+                System.out.print("-");
             }
+            System.out.println(r);
+        }
+    }
+    
+    public static void problemE(){
+        for(int r = 7; r>=1; r--){
+            for(int c = 0; c < r; c++){
+                System.out.print(" ");
+            }
+            for(int c = 7; c>=r; c--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void problemF(){
+        probFUpper();
+        probFLower();
+    }
+    
+    public static void probFUpper(){
+        for (int r = 0; r < 6; r++) {
+             for (int spaces = 0; spaces < 6 - r; spaces++) {
+                 System.out.print(" ");
+             }
+             for (int stars = 0; stars < 1 + 2 * r; stars++) {
+                 System.out.print("*");
+             }
+             System.out.println();
+        }
+    }
+    
+    public static void probFLower(){
+        for (int r = 6; r>=0; r--) {
+             for (int spaces = 0; spaces < 6 - r; spaces++) {
+                 System.out.print(" ");
+             }
+             for (int stars = 0; stars < 1 + 2 * r; stars++) {
+                 System.out.print("*");
+             }
+             System.out.println();
+        }
+    }
+    
+    
+    //Don't know
+    
+    public static void problemG(){
+        for (int r = 0; r < 4; r++) {
+             for (int spaces = 0; spaces < 4 - r; spaces++) {
+                 System.out.print(" ");
+             }
+             for (int slashes = 0; slashes < 1 + 2 * r; slashes++) {
+                 System.out.print("/");
+             }
+             System.out.println();
         }
     }
     
