4009a36dadb38683891bc30ac10cbd308a2c20f5
Peter Klopp <795294@seq.org>
2019-09-26 09:34:32 -0700

    PersonRunner

--- Labs/Lab924 ParseName/Person.java
+++ Labs/Lab924 ParseName/Person.java
@@ -19,15 +19,32 @@ public class Person
     public void parseName(String fullName){
         int comma = fullName.indexOf(",");
         
-        if(comma != -1){
+        //format 1
+        if(comma != -1 && fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
             lName = fullName.substring(0,fullName.indexOf(","));
-            fName = fullName.substring(fullName.indexOf(",")+2, fullName.lastIndexOf(" "));
+            fName = fullName.substring(fullName.indexOf(",")+1, fullName.lastIndexOf(" "));
             mName = fullName.substring(fullName.lastIndexOf(" "));
-        } else {
+        } 
+        
+        //format 2
+        if(comma != -1 && fullName.indexOf(" ") == fullName.lastIndexOf(" ")){
+            lName = fullName.substring(0,fullName.indexOf(","));
+            fName = fullName.substring(fullName.indexOf(",")+1);
+        }
+        
+        //format 3
+        if(comma == -1 && fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
             fName = fullName.substring(0, fullName.indexOf(" "));
             lName = fullName.substring(fullName.lastIndexOf(" "));
             mName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" "));
         }
+        
+        //format 4
+        if(comma == -1 && fullName.indexOf(" ") == fullName.lastIndexOf(" ")){
+            fName = fullName.substring(0, fullName.indexOf(" "));
+            lName = fullName.substring(fullName.lastIndexOf(" "));
+        }
+        
     }
 
 }
