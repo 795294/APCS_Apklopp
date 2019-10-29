9034ae0c060ff85a5022748ccbcc141e80174003
795294 <795294@seq.org>
2019-10-29 09:26:05 -0700

    gh

--- Projects/StudentList/StudList.java
+++ Projects/StudentList/StudList.java
@@ -11,14 +11,89 @@ public class StudList
 {
     ArrayList<Student> studList = new ArrayList<Student>();
 
-    
+    private String fName = "";
+    private String mName = "";
+    private String lName = "";
+
     public StudList()
     {
-       
+
+    }
+
+    public void addStudent(String fullName){
+        int comma = fullName.indexOf(",");
+
+        //format 1
+        if(comma != -1 && fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
+            lName = fullName.substring(0,fullName.indexOf(","));
+            fName = fullName.substring(fullName.indexOf(",")+1, fullName.lastIndexOf(" "));
+            mName = fullName.substring(fullName.lastIndexOf(" "));
+        } 
+
+        //format 2
+        if(comma != -1 && fullName.indexOf(" ") == fullName.lastIndexOf(" ")){
+            lName = fullName.substring(0,fullName.indexOf(","));
+            fName = fullName.substring(fullName.indexOf(",")+1);
+        }
+
+        //format 3
+        if(comma == -1 && fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
+            fName = fullName.substring(0, fullName.indexOf(" "));
+            lName = fullName.substring(fullName.lastIndexOf(" "));
+            mName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" "));
+        }
+
+        //format 4
+        if(comma == -1 && fullName.indexOf(" ") == fullName.lastIndexOf(" ")){
+            fName = fullName.substring(0, fullName.indexOf(" "));
+            lName = fullName.substring(fullName.lastIndexOf(" "));
+        }
     }
     
-    public void addStudent(String fullName){
+    public void deleteStudent(String lastName){
+        for(int i  = 0; i < studList.size(); i++){
+            ///if(studList.get(i).indexOf(lastName) != -1){
+               //studList.remove(i);
+            //}
+        }
         
     }
+    
+    public void deleteStudent(int stuNumber){
+        for(int i  = 0; i < studList.size(); i++){
+            //if(studList.get(i).stuNumber == stuNumber){
+            //}
+        }
+    }
+    
+    public void editStudentList(int stuNumber){
+        
+    }
+    
+    public void clearList(){
+        
+    }
+    
+    public void printList(){
+        
+    }
+    
+    public void printStudent(){
+        
+    }
+    
+    public void sortStudents(String lastName){
+        
+    }
+    
+    public void sortStudents(int stuNumber){
+        
+    }
+    
+    public ArrayList<Student> filterSearchStudentList(String key){
+        
+    }
+    
+    
 
 }
