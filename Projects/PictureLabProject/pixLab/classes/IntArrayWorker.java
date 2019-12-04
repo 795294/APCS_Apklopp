9e1f51db25440b2060f6cdaef8ad957bb1ac2c74
795294 <795294@seq.org>
2019-12-04 10:19:32 -0800

    Lab1204

--- Projects/PictureLabProject/pixLab/classes/IntArrayWorker.java
+++ Projects/PictureLabProject/pixLab/classes/IntArrayWorker.java
@@ -1,5 +1,7 @@
 package classes;
 
+import java.util.*;
+
 public class IntArrayWorker
 {
     /** two dimensional matrix */
@@ -28,27 +30,28 @@ public class IntArrayWorker
 
     public int getLargest(){
         int maxValue = matrix[0][0];
-        
+
         for(int r = 0; r < matrix.length; r++){
             for(int c = 0; c<matrix[r].length; c++){
                 if(matrix[r][c] > maxValue){
-                   maxValue = matrix[r][c];
+                    maxValue = matrix[r][c];
                 }
             }
         }
-        
+
         return maxValue;
-        
+
     }
-    
+
     public int getColTotal(int colVal){
         int count = 0;
         for(int i = 0; i<matrix.length; i++){
             count += matrix[i][colVal];
         }
-        
+
         return count;
     }
+
     /**
      * Method to return the total 
      * @return the total of the values in the array
@@ -134,6 +137,75 @@ public class IntArrayWorker
                     matrix[row][col] = 3;
             }
         }
+    }
+
+    public int[][] loadEvensOdds(int[][] mat){
+        int count;
+        for(int r = 0; r< mat.length; r++){
+            for(int c = 0; c<mat[r].length/2; c++){
+
+                count = (int)((Math.random()*8)+1);
+
+                if(count%2 == 0){
+                    mat[r][c] = count;
+
+                    mat[r][mat[r].length-1-c] = count+1;
+                } else {
+                    mat[r][mat[r].length-1-c] = count;
+
+                    mat[r][c] = count+1;
+                }
+
+            }
+        }
+       
+        
+        for(int r = 0; r< mat.length; r++){
+            for(int c = 0; c<mat[r].length; c++){
+                System.out.print(mat[r][c]+",");
+            }
+
+            System.out.println();
+        }
+
+        return mat;
+    }
+
+    public int[][] flipVertical(int[][] mat){
+        for(int r = 0; r< mat.length; r++){
+            for(int c = 0; c<mat[r].length/2; c++){
+                
+                int temp = mat[r][c];
+                        
+                mat[r][c] = mat[r][mat[r].length-1-c];
+                        
+                mat[r][mat[r].length-1-c] = temp;
+                
+            }
+        }
+        
+        System.out.println();
+        
+        
+        
+        for(int r = 0; r< mat.length; r++){
+            for(int c = 0; c<mat[r].length; c++){
+                System.out.print(mat[r][c]+",");
+            }
+
+            System.out.println();
+        }
+        
+        return mat;
+    }
+
+    public static void main(){
+        IntArrayWorker arr = new IntArrayWorker();
+
+        int[][] mat = new int[10][10];
+       
+        
+        arr.flipVertical(arr.loadEvensOdds(mat));
     }
 
 }
--- Projects/PictureLabProject/pixLab/classes/Picture.java
+++ Projects/PictureLabProject/pixLab/classes/Picture.java
@@ -205,6 +205,23 @@ public class Picture extends SimplePicture
             }
         } 
     }
+    
+    public void mirrorHorizontal(){
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel topPixel = null;
+        Pixel bottomPixel = null;
+        int width = pixels[0].length;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; col < width / 2; col++)
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][width - 1 - col];
+                leftPixel.setColor(rightPixel.getColor());
+            }
+        } 
+        
+    }
 
     /** Mirror just part of a picture of a temple */
     public void mirrorTemple()
